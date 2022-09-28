c5ed4318402b3088e2ca2aa0473f2c04ea7362cf
Datong Sun <datong.sun@konghq.com>
2022-09-27 21:02:39 -0700

    style(*) fix clippy warnings and errors

--- src/ffi.rs
+++ src/ffi.rs
@@ -64,34 +64,32 @@ pub extern "C" fn schema_new() -> *mut Schema {
 }
 
 #[no_mangle]
-pub extern "C" fn schema_free(schema: *mut Schema) {
-    unsafe { Box::from_raw(schema) };
+pub unsafe extern "C" fn schema_free(schema: *mut Schema) {
+    drop(Box::from_raw(schema));
 }
 
 #[no_mangle]
-pub extern "C" fn schema_add_field(schema: &mut Schema, field: *const i8, typ: Type) {
-    let field = unsafe {
-        ffi::CStr::from_ptr(field as *const c_char)
-            .to_str()
-            .unwrap()
-    };
+pub unsafe extern "C" fn schema_add_field(schema: &mut Schema, field: *const i8, typ: Type) {
+    let field = ffi::CStr::from_ptr(field as *const c_char)
+        .to_str()
+        .unwrap();
 
     schema.add_field(field, typ)
 }
 
 #[no_mangle]
-pub extern "C" fn router_new(schema: &Schema) -> *mut Router {
+pub unsafe extern "C" fn router_new(schema: &Schema) -> *mut Router {
     Box::into_raw(Box::new(Router::new(schema)))
 }
 
 #[no_mangle]
-pub extern "C" fn router_free(router: *mut Router) {
-    unsafe { Box::from_raw(router) };
+pub unsafe extern "C" fn router_free(router: *mut Router) {
+    drop(Box::from_raw(router));
 }
 
 #[no_mangle]
 // uuid must be ASCII representation of 128-bit UUID
-pub extern "C" fn router_add_matcher(
+pub unsafe extern "C" fn router_add_matcher(
     router: &mut Router,
     priority: usize,
     uuid: *const i8,
@@ -99,17 +97,15 @@ pub extern "C" fn router_add_matcher(
     errbuf: *mut u8,
     errbuf_len: *mut usize,
 ) -> bool {
-    let uuid = unsafe { ffi::CStr::from_ptr(uuid as *const c_char).to_str().unwrap() };
-    let atc = unsafe { ffi::CStr::from_ptr(atc as *const c_char).to_str().unwrap() };
-    let errbuf = unsafe { from_raw_parts_mut(errbuf, ERR_BUF_MAX_LEN) };
+    let uuid = ffi::CStr::from_ptr(uuid as *const c_char).to_str().unwrap();
+    let atc = ffi::CStr::from_ptr(atc as *const c_char).to_str().unwrap();
+    let errbuf = from_raw_parts_mut(errbuf, ERR_BUF_MAX_LEN);
 
     let uuid = Uuid::try_parse(uuid).expect("invalid UUID format");
 
     if let Err(e) = router.add_matcher(priority, uuid, atc) {
         errbuf[..e.len()].copy_from_slice(e.as_bytes());
-        unsafe {
-            *errbuf_len = e.len();
-        }
+        *errbuf_len = e.len();
         return false;
     }
 
@@ -118,34 +114,34 @@ pub extern "C" fn router_add_matcher(
 
 #[no_mangle]
 // uuid must be ASCII representation of 128-bit UUID
-pub extern "C" fn router_remove_matcher(
+pub unsafe extern "C" fn router_remove_matcher(
     router: &mut Router,
     priority: usize,
     uuid: *const i8,
 ) -> bool {
-    let uuid = unsafe { ffi::CStr::from_ptr(uuid as *const c_char).to_str().unwrap() };
+    let uuid = ffi::CStr::from_ptr(uuid as *const c_char).to_str().unwrap();
     let uuid = Uuid::try_parse(uuid).expect("invalid UUID format");
 
     router.remove_matcher(priority, uuid)
 }
 
 #[no_mangle]
-pub extern "C" fn router_execute(router: &Router, context: &mut Context) -> bool {
+pub unsafe extern "C" fn router_execute(router: &Router, context: &mut Context) -> bool {
     router.execute(context)
 }
 
 #[no_mangle]
-pub extern "C" fn router_get_fields(
+pub unsafe extern "C" fn router_get_fields(
     router: &Router,
     fields: *mut *const u8,
     fields_len: *mut usize,
 ) -> usize {
     if !fields.is_null() {
         assert!(!fields_len.is_null());
-        assert!(unsafe { *fields_len } >= router.fields.len());
+        assert!(*fields_len >= router.fields.len());
 
-        let fields = unsafe { from_raw_parts_mut(fields, *fields_len) };
-        let fields_len = unsafe { from_raw_parts_mut(fields_len, *fields_len) };
+        let fields = from_raw_parts_mut(fields, *fields_len);
+        let fields_len = from_raw_parts_mut(fields_len, *fields_len);
 
         for (i, k) in router.fields.keys().enumerate() {
             fields[i] = k.as_bytes().as_ptr();
@@ -157,36 +153,32 @@ pub extern "C" fn router_get_fields(
 }
 
 #[no_mangle]
-pub extern "C" fn context_new(schema: &Schema) -> *mut Context {
+pub unsafe extern "C" fn context_new(schema: &Schema) -> *mut Context {
     Box::into_raw(Box::new(Context::new(schema)))
 }
 
 #[no_mangle]
-pub extern "C" fn context_free(context: *mut Context) {
-    unsafe { Box::from_raw(context) };
+pub unsafe extern "C" fn context_free(context: *mut Context) {
+    drop(Box::from_raw(context));
 }
 
 #[no_mangle]
-pub extern "C" fn context_add_value(
+pub unsafe extern "C" fn context_add_value(
     context: &mut Context,
     field: *const i8,
     value: &CValue,
     errbuf: *mut u8,
     errbuf_len: *mut usize,
 ) -> bool {
-    let field = unsafe {
-        ffi::CStr::from_ptr(field as *const c_char)
-            .to_str()
-            .unwrap()
-    };
-    let errbuf = unsafe { from_raw_parts_mut(errbuf, ERR_BUF_MAX_LEN) };
+    let field = ffi::CStr::from_ptr(field as *const c_char)
+        .to_str()
+        .unwrap();
+    let errbuf = from_raw_parts_mut(errbuf, ERR_BUF_MAX_LEN);
 
     let value: Result<Value, _> = value.try_into();
     if let Err(e) = value {
         errbuf[..e.len()].copy_from_slice(e.as_bytes());
-        unsafe {
-            *errbuf_len = e.len();
-        }
+        *errbuf_len = e.len();
         return false;
     }
 
@@ -196,7 +188,7 @@ pub extern "C" fn context_add_value(
 }
 
 #[no_mangle]
-pub extern "C" fn context_get_result(
+pub unsafe extern "C" fn context_get_result(
     context: &Context,
     uuid_hex: *mut u8,
     matched_field: *const i8,
@@ -212,41 +204,37 @@ pub extern "C" fn context_get_result(
     }
 
     if !uuid_hex.is_null() {
-        let uuid_hex = unsafe { from_raw_parts_mut(uuid_hex, Hyphenated::LENGTH) };
+        let uuid_hex = from_raw_parts_mut(uuid_hex, Hyphenated::LENGTH);
         let res = context.result.as_ref().unwrap();
 
         res.uuid.as_hyphenated().encode_lower(uuid_hex);
 
         if !matched_field.is_null() {
-            let matched_field = unsafe {
-                ffi::CStr::from_ptr(matched_field as *const c_char)
-                    .to_str()
-                    .unwrap()
-            };
+            let matched_field = ffi::CStr::from_ptr(matched_field as *const c_char)
+                .to_str()
+                .unwrap();
             assert!(!matched_value.is_null());
             assert!(!matched_value_len.is_null());
             if let Some(Value::String(v)) = res.matches.get(matched_field) {
-                unsafe { *matched_value = v.as_bytes().as_ptr() };
-                unsafe { *matched_value_len = v.len() };
+                *matched_value = v.as_bytes().as_ptr();
+                *matched_value_len = v.len();
             } else {
-                unsafe { *matched_value_len = 0 };
+                *matched_value_len = 0;
             }
         }
 
         if !context.result.as_ref().unwrap().captures.is_empty() {
-            assert!(unsafe { *capture_names_len } >= res.captures.len());
-            assert!(unsafe { *capture_names_len == *capture_values_len });
+            assert!(*capture_names_len >= res.captures.len());
+            assert!(*capture_names_len == *capture_values_len);
             assert!(!capture_names.is_null());
             assert!(!capture_names_len.is_null());
             assert!(!capture_values.is_null());
             assert!(!capture_values_len.is_null());
 
-            let capture_names = unsafe { from_raw_parts_mut(capture_names, *capture_names_len) };
-            let capture_names_len =
-                unsafe { from_raw_parts_mut(capture_names_len, *capture_names_len) };
-            let capture_values = unsafe { from_raw_parts_mut(capture_values, *capture_values_len) };
-            let capture_values_len =
-                unsafe { from_raw_parts_mut(capture_values_len, *capture_values_len) };
+            let capture_names = from_raw_parts_mut(capture_names, *capture_names_len);
+            let capture_names_len = from_raw_parts_mut(capture_names_len, *capture_names_len);
+            let capture_values = from_raw_parts_mut(capture_values, *capture_values_len);
+            let capture_values_len = from_raw_parts_mut(capture_values_len, *capture_values_len);
 
             for (i, (k, v)) in res.captures.iter().enumerate() {
                 capture_names[i] = k.as_bytes().as_ptr();
