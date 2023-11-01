0b9c7f5610140438ab1aacaaa4fe509273794866
Datong Sun <datong.sun@konghq.com>
2023-11-01 18:09:34 +0800

    docs(lib): add features documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+/*!
+This crate provides a powerful rule based matching engine that can match a set of routes
+against dynamic input value efficiently.
+It is mainly used inside the [Kong Gateway](https://github.com/Kong/kong)
+for performing route matching against incoming requests and is used as a FFI binding
+for LuaJIT.
+
+Please see the [repositoty README.md](https://github.com/Kong/atc-router/blob/main/README.md)
+for more detailed explainations of the concepts and APIs.
+
+# Crate features
+
+* **ffi** -
+  Builds the FFI based interface which is suitable for use by a foreign language such as
+  C or LuaJIT. This feature is on by default.
+* **serde** -
+  Enable serde integration which allows data structures to be serializanle/deserializable.
+*/
+
 pub mod ast;
 pub mod context;
 pub mod interpreter;
