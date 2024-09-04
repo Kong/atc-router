82523da26d522494a7bb0e651f668da12488b298
Qi <add_sp@outlook.com>
2024-09-04 11:49:19 +0800

    tests(benches): add a script to benchmark the router building

---
+++ benches/build.rs
@@ -0,0 +1,35 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use atc_router::router::Router;
+use atc_router::schema::Schema;
+use atc_router::ast::{Type, Value};
+use atc_router::context::Context;
+use uuid::Uuid;
+
+// To run this benchmark, execute the following command:
+// ```shell
+// cargo bench --bench build
+// ```
+
+const N : usize = 1000;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut schema = Schema::default();
+    schema.add_field("a", Type::Int);
+
+    let mut context = Context::new(&schema);
+    context.add_value("a", Value::Int(N as i64));
+
+    c.bench_function("Build Router", |b| {
+        b.iter_with_large_drop(|| {
+            let mut router = Router::new(&schema);
+            for i in 0..N {
+                let expr = format!("((a > 0 || a < {}) && a != 0) && a == 1", N + 1);
+                router.add_matcher(N - i, Uuid::new_v4(), &expr).unwrap();
+            }
+            router
+        });
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
