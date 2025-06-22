d7383659f26c55e7d97f0f6b1257dcd0933e788e
chronolaw <chrono_cpp@me.com>
2025-06-22 14:12:11 +0800

    clean

--- benches/build.rs
+++ benches/build.rs
@@ -16,25 +16,28 @@ fn make_uuid(a: usize) -> String {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut schema = Schema::default();
-    schema.add_field("a", Type::Int);
-
     // prepare test data
     let mut data = Vec::new();
     for i in 0..N {
-        let expr = format!("((a > 0 || a < {}) && a != 0) && a == 1", N + 1);
-        let variant = make_uuid(i);
-        let uuid = Uuid::try_from(variant.as_str()).unwrap();
+        let priority = N - i;
 
-        data.push((uuid, expr))
+        let uuid = make_uuid(i);
+        let uuid = Uuid::try_from(uuid.as_str()).unwrap();
+
+        let expr = format!("((a > 0 || a < {}) && a != 0) && a == 1", N + 1);
+
+        data.push((priority, uuid, expr))
     }
+
+    let mut schema = Schema::default();
+    schema.add_field("a", Type::Int);
 
     c.bench_function("Build Router", |b| {
         b.iter_with_large_drop(|| {
             let mut router = Router::new(&schema);
             for i in 0..N {
                 let v = &data[i];
-                router.add_matcher(N - i, v.0, &v.1).unwrap();
+                router.add_matcher(v.0, v.1, &v.2).unwrap();
             }
             router
         });
