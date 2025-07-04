ef19eba5abd1af946f57e43d1dcfddcfb5536835
Luca BRUNO <lucab@lucabruno.net>
2025-07-04 15:52:28 +0200

    poc

--- src/router.rs
+++ src/router.rs
@@ -11,8 +11,23 @@ use uuid::Uuid;
 struct MatcherKey(usize, Uuid);
 
 #[derive(Debug)]
+enum SchemaOwnedOrRef<'a> {
+    Ref(&'a Schema),
+    Owned(Schema),
+}
+
+impl AsRef<Schema> for SchemaOwnedOrRef<'_> {
+    fn as_ref(&self) -> &Schema {
+        match self {
+            SchemaOwnedOrRef::Ref(s) => s,
+            SchemaOwnedOrRef::Owned(s) => s,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Router<'a> {
-    schema: &'a Schema,
+    schema: SchemaOwnedOrRef<'a>,
     matchers: BTreeMap<MatcherKey, Expression>,
     pub fields: HashMap<String, usize>,
 }
@@ -20,10 +35,22 @@ pub struct Router<'a> {
 impl<'a> Router<'a> {
     pub fn new(schema: &'a Schema) -> Self {
         Self {
-            schema,
+            schema: SchemaOwnedOrRef::Ref(schema),
             matchers: BTreeMap::new(),
             fields: HashMap::new(),
         }
+    }
+
+    pub fn new_owning(schema: Schema) -> Self {
+        Self {
+            schema: SchemaOwnedOrRef::Owned(schema),
+            matchers: BTreeMap::new(),
+            fields: HashMap::new(),
+        }
+    }
+
+    pub fn schema(&self) -> &Schema {
+        self.schema.as_ref()
     }
 
     pub fn add_matcher(&mut self, priority: usize, uuid: Uuid, atc: &str) -> Result<(), String> {
@@ -35,7 +62,7 @@ impl<'a> Router<'a> {
 
         let ast = parse(atc).map_err(|e| e.to_string())?;
 
-        ast.validate(self.schema)?;
+        ast.validate(self.schema.as_ref())?;
         ast.add_to_counter(&mut self.fields);
 
         assert!(self.matchers.insert(key, ast).is_none());
@@ -144,5 +171,19 @@ mod tests {
         let mut ctx = Context::new(&schema);
         ctx.add_value("http.path", "/not-dev".to_owned().into());
         router.try_match(&ctx).ok_or(()).expect_err("should fail");
+    }
+
+    #[test]
+    fn test_basic_owned_schema() {
+        let mut schema = Schema::default();
+        schema.add_field("http.path", Type::String);
+
+        let mut router: Router<'static> = Router::new_owning(schema);
+        router
+            .add_matcher(0, Uuid::default(), "http.path == \"/dev\"")
+            .expect("should add");
+        let mut ctx = Context::new(router.schema());
+        ctx.add_value("http.path", "/dev".to_owned().into());
+        router.try_match(&ctx).expect("matches");
     }
 }
